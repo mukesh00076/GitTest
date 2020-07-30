22e461b79ccc2306c84b925856e0d736ed943169
mukesh00076 <50291186+mukesh00076@users.noreply.github.com>
2020-07-30 19:59:17 +0530

    My First For Program
    My First For Program

---
+++ MyLoop.py
@@ -0,0 +1,13 @@
+My Fisrt Python Program
+i = 100
+j = 200
+
+for i < j:
+  print("Addition of i + j :", i + j)
+  i = i + 10 
+    if i == j:
+      print("Equal")
+    elif:
+      print("Not Equal")
+      
+      
